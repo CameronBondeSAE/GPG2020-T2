765f30a603b2c8095574eeb9989bc7b19695388a
AJ <1018212@student.sae.edu.au>
2020-06-18 09:22:36 +1000

    AI random movement, on collision destroys object

---
+++ Assets/Student workspace/AlexB/DestroyOnCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
