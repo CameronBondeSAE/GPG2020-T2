8a64e4b90d55500c01316a861387f1aff02295e5
ayounes2001 <1016744@student.sae.edu.au>
2020-06-18 11:48:48 +1000

    Updated A* Algorithm

--- Assets/Student workspace/Anthony Younes WS/A Star Testing/Scripts/AStarTest.cs
+++ Assets/Student workspace/Anthony Younes WS/A Star Testing/Scripts/AStarTest.cs
@@ -1,52 +1,111 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
+using Boo.Lang;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class AStarTest : MonoBehaviour
+namespace AnthonyY
 {
-    public LayerMask unTouchableSurface; //mask for obstacles
-    
-    //lists of nodes to follow to get to the objective
-    public Node[] nodeArray; 
-    public float nodeDiameter;
-    public float nodeRadius;
-    
-    //the grid itself
-    //things that it may we need to take into the account
-    public Vector2 gridSize;
-    public int gridSizex;
-    public int gridSizeY;
-    
-    //I'm not 100% sure of these lists but can be used to see what nodes have already been checked
-    public List<Node> currentNode;
-    public List<Node> neighbourNodes;
-    public List<Node> openNodes;
-    
-   
-   //TODO
-   //A GIZMOS to see the line update ingame
-   public void OnDrawGizmos(Node targetNode)
-   {
-       Gizmos.color = Color.yellow;
-   }
-
-   void Start()
+    public class AStarTest : MonoBehaviour
     {
+        public LayerMask unTouchableSurface; //mask for obstacles
+    
+        //lists of nodes to follow to get to the objective
+        public Node[] nodeArray; 
+        public float nodeDiameter;
+        public float nodeRadius;
+    
+        //the grid itself
+        //things that it may we need to take into the account
+        public Vector2 gridSize;
+        public int gridSizex;
+        public int gridSizeY;
+    
+        //I'm not 100% sure of these lists but can be used to see what nodes have already been checked
+        public List<Node> closedNodes;
+        public List<Node> openNodes; //nodes that have been visited but not been expanded
+        public List<Edge> neighbourNodes; //adjacency 
+    
+        //need a goal variable
+        //start position
+        public Node nodeStart;
+        public Node nodeGoal;
+        public Node currentNode;
         
-    }
+        //maybe put all cost variables 
+        public float h_cost;
+        private float f_cost;
+        private float g_cost;
 
-    
 
-    public void NodeGrid()
-    {
-        foreach (Node n in nodeArray)
+        //TODO
+        //A GIZMOS to see the line update ingame
+        public void OnDrawGizmos()
         {
-            
+            Gizmos.color = Color.yellow;
         }
 
-        
-    }
+        void Start()
+        {
+            openNodes.Add(nodeStart);
+            openNodes.Add(nodeGoal);
+            openNodes.Add();
+           
+        }
+
+        public void calculateFCost()
+        {
+            h_cost = Math.Sqrt(nodeStart - gridSizex.x) + 
+        }
     
+
+        public void nodeSearch()
+        {
+            while (openNodes.Count > 0)
+            {
+                //currentNode equals lowest f cost
+                //remove currentNode from open List
+                //add it to closed List
+                
+                
+                
+                foreach (Node n in nodeArray)
+                {
+                    if (n.Equals(currentNode))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+                
+                
+                if (currentNode.Equals(nodeGoal))
+                {
+                    Debug.Log("We have reached the goal");
+                    //yay we made it
+                
+                }
+                else(!currentNode.Equals(nodeGoal))
+                {
+                    Debug.Log("Cannot find path");
+                }
+                
+                
+            }
+            
+            
+
+           
+           
+          
+            
+        }
+        
+
+    }
+
 }
