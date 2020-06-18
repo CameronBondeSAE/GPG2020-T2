2703bbf677e3a44e73693dac080c14b8075d7c39
jordanhanna0 <52388379+jordanhanna0@users.noreply.github.com>
2020-06-18 15:37:23 +1000

    Commented out code, draft

--- Assets/Student workspace/Jordan/Test.cs
+++ Assets/Student workspace/Jordan/Test.cs
@@ -7,29 +7,29 @@ public class Test : MonoBehaviour
 	public LayerMask Colliable;
 	public Vector2 gridWorldSize;
 	public float nodeRadius;
-	Node[,] grid;
+	//Node[,] grid;
 
-	void Awake()
-	{
-		nodeDiameter = nodeRadius * 2;
-		gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
-		gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
-		CreateGrid();
-	}
+	//void Awake()
+	//{
+	//	nodeDiameter = nodeRadius * 2;
+	//	gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+	//	gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+	//	CreateGrid();
+	//}
 
-	void CreateGrid()
-	{
-		grid = new Node[gridSizeX, gridSizeY];
-		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+	//void CreateGrid()
+	//{
+	//	grid = new Node[gridSizeX, gridSizeY];
+	//	Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
 
-		for (int x = 0; x < gridSizeX; x++)
-		{
-			for (int y = 0; y < gridSizeY; y++)
-			{
-				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
-				bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
-				grid[x, y] = new Node(walkable, worldPoint, x, y);
-			}
-		}
-	}
+	//	for (int x = 0; x < gridSizeX; x++)
+	//	{
+	//		for (int y = 0; y < gridSizeY; y++)
+	//		{
+	//			Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+	//			bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+	//			grid[x, y] = new Node(walkable, worldPoint, x, y);
+	//		}
+	//	}
+	//}
 }
