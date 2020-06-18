e5cb04944a47e01f626fa82199018a57e4492050
Niall <1013734@student.sae.edu.au>
2020-06-18 15:19:43 +1000

    A * so far, So fuck all

---
+++ Assets/Student workspace/Niall/AStarShiz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class AStarShiz : MonoBehaviour
+{
+    public bool Open;
+    
+    public float HCost;
+    public float GCost;
+    public float FCost;
+    
+    
+}
