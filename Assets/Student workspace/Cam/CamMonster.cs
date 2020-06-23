4c479b76d816faced07c3bdacc1864e661e86b53
Cameron Bonde <c.bonde@sae.edu>
2020-06-23 17:25:19 +1000

    Tests for Unity State machine

---
+++ Assets/Student workspace/Cam/CamMonster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMonster : MonoBehaviour
+{
+	public Animator animator;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// TODO: Put in the real player distance (multiple players?)
+        animator.SetFloat("DistanceToPlayer", Vector3.Distance(transform.position, Vector3.zero));
+    }
+}
---
+++ Assets/Student workspace/Cam/WanderState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderState : StateMachineBehaviour
+{
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		base.OnStateEnter(animator, stateInfo, layerIndex);
+		
+		Debug.Log("Wander state enter");
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		base.OnStateUpdate(animator, stateInfo, layerIndex);
+		
+		Debug.Log("Wander state update");
+	}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		base.OnStateExit(animator, stateInfo, layerIndex);
+		
+		Debug.Log("Wander state exit");
+	}
+}
