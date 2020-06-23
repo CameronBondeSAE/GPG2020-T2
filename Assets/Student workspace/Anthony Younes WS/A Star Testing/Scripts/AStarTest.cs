8e8552282bf0aae998fbc32bfb8bb3d4c15ddbd8
ayounes2001 <1016744@student.sae.edu.au>
2020-06-23 16:51:34 +1000

    Doing some neighbour code stuff for A*

--- Assets/Student workspace/Anthony Younes WS/A Star Testing/Scripts/AStarTest.cs
+++ Assets/Student workspace/Anthony Younes WS/A Star Testing/Scripts/AStarTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Linq;
+using System.Xml;
 using Boo.Lang;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
@@ -9,130 +10,131 @@ namespace AnthonyY
 {
     public class AStarTest : MonoBehaviour
     {
-        public class NodeInfo
+        public class NodeInfo //cost variables
         {
-            //maybe put all cost variables 
-            public float h;
-            public float f;
-            public float g = 0;
-            public float normalCost = 1;
-            public double diagonalCost = 1.4;
-            
-            
+            public int h;
+            public int f;
+            public int g = 0;
+            public int normalCost = 1;
+            public float diagonalCost = 1.4f;
+
             public Node parent;
 
             //calculating the heureustic cost
             public float fCost
             {
                 get { return g + h; }
-
-
-
             }
+        }
+        //TODO
+        //A GIZMOS to see the line update ingame
+        public void OnDrawGizmos()
+        {
+            //Gizmos.color = Color.yellow;
+        }
 
-            public LayerMask unTouchableSurface; //mask for obstacles
-            //the grid itself
-            //things that it may we need to take into the account
 
-            public Vector2Int gridSize;
+        public LayerMask unTouchableSurface; //mask for obstacles
+            private Vector2Int gridSize;
 
             public int gridSizeX, gridSizeY;
 
             //lists of nodes to follow to get to the objective
-            public Node[,] nodeArray;
-            public float nodeDiameter;
-
-            //I'm not 100% sure of these lists but can be used to see what nodes have already been checked
-            public List<Node> closedNodes;
-            public List<Node> openNodes; //nodes that have been visited but not been expanded
+            private Node[,] nodeArray;
+            private float nodeDiameter;
+            public float nodeRadius;
+            private List<Node> closedNodes;
+            private List<Node> openNodes; //nodes that have been visited but not been expanded
 
             //need a goal variable
             //start position
-            public Node currentNode;
+            private Node currentNode;
 
-          
+            private Node nodeStart;
+            private Node nodeGoal;
 
-
-            //TODO
-            //A GIZMOS to see the line update ingame
-            public void OnDrawGizmos()
-            {
-                //Gizmos.color = Color.yellow;
-            }
 
             void Awake()
             {
+                nodeDiameter = nodeRadius * 2;
                 gridSizeX = Mathf.RoundToInt(gridSize.x / nodeDiameter);
                 gridSizeY = Mathf.RoundToInt(gridSize.y / nodeDiameter);
+                CreateNodes();
             }
 
-            public void nodeSearch(Node nodeStart, Node nodeGoal)
+            public void CreateNodes()
             {
-                openNodes.Add(nodeStart);
-                openNodes.Add(nodeGoal);
                 nodeArray = new Node[gridSize.x, gridSize.y];
+                while (openNodes.Count > 0)
+                {
+                    openNodes.Add(nodeStart);
+                    openNodes.Add(nodeGoal);
+                }
                 for (int x = 0; x < gridSize.x; x++)
                 {
                     for (int y = 0; y < gridSize.y; y++)
                     {
                         Vector2Int gridPos = new Vector2Int(gridSize.x, gridSize.y);
-                        parent = currentNode;
-                        while (openNodes.Count > 0)
+                        //currentNode equals lowest f cost
+                        //remove currentNode from open List
+                        //add it to closed List
+                        foreach (Node n in nodeArray)
                         {
-                            //currentNode equals lowest f cost
-                            //remove currentNode from open List
-                            //add it to closed List
-                            foreach (Node n in nodeArray)
-                            {
-                                //goal isnt found
-                                if (n.Equals(currentNode))
-                                {
-                                    openNodes.Remove(currentNode);
-                                    closedNodes.Add(currentNode);
-                                    continue;
 
-                                }
-                            }
-                            //goal is found
-                            if (currentNode.Equals(nodeGoal))
+                            //goal isnt found
+                            if (n.Equals(currentNode))
                             {
-                                Debug.Log("We have reached the goal");
-
+                                openNodes.Remove(currentNode);
+                                closedNodes.Add(currentNode);
+                                continue;
                             }
-                            /*else(!currentNode.Equals(nodeGoal))
-                            {
-                                Debug.Log("Cannot find path");
-                            }*/
                         }
-                    }
 
-                }
-            }
-
-            public void neighbourSearch(Node node)
-            {
-
-                //check for borders you cant check for in an array
-                //2d array grid
-                for (int x = 0; x < -1; x++)
-                {
-                    for (int y = 0; y < -1; y++)
-                    {
-                        if(x ==0 & y ==0)
+                        //goal is found
+                        if (currentNode.Equals(nodeGoal))
                         {
-                            continue;
+                            Debug.Log("We have reached the goal");
+                            break;
                         }
-                        //CHECK FOR BORDERS 
-                        //I GUESS check from the bottom left of the grid
-                        
+
+                        /*else(!currentNode.Equals(nodeGoal))
+                        {
+                            Debug.Log("Cannot find path");
+                        }*/
                     }
-                    
                 }
-
-
             }
             
+/// <summary>
+/// This part of the code checks to see neighbour nodes and add them to a list when they get checked
+/// </summary>
+/// <param name="node"></param>
+/// <returns></returns>
+            public List<Node> NeighbourSearch(Node node)
+            {
+                List<Node> neighbours = new List<Node>();
+                //check for borders you cant check for in an array
+                    //2d array grid
+                    for (int x = -1; x < 1; x++)
+                    {
+                        for (int y = -1; y < 1; y++)
+                        {
+                            if (x == 0 & y == 0)
+                            {
+                                continue;
+                                int checkX = node.gridX + x;
+                                int checkY = node.gridY + y;
+
+                                if (checkX < gridSizeX && checkX < 0 && checkY < 0 && checkY < gridSizeY)
+                                {
+                                    neighbours.Add(nodeArray[checkX,checkY]);
+                                }
+                            }
+                        }
+                    }
+                    return neighbours;
+            }
         }
 
-    }
+  
 }
