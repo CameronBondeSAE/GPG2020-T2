48b4fad641e62818990c35220635874541d1efc4
ayounes2001 <1016744@student.sae.edu.au>
2020-06-30 13:19:59 +1000

    A start on making an inventory

---
+++ Assets/Student workspace/Anthony Younes WS/ActionMap/Scripts/InventoryBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AnthonyY
+{
+    public class InventoryBase : MonoBehaviour
+    {
+        public List<GameObject> inventoryItems;
+        protected virtual void Inventory()
+        {
+            
+        }
+    }
+ 
+}
