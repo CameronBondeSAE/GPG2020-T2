80de995be42d2e97c9c6d16dfa4226f31422d62d
Niall <1013734@student.sae.edu.au>
2020-06-30 16:42:24 +1000

    A* pathfinding + spawnManager prefab

--- Assets/Student workspace/Niall/Scripts/Node.cs
+++ Assets/Student workspace/Niall/Scripts/Node.cs
@@ -8,11 +8,26 @@ namespace Niall
     {
         public bool walkable;
         public Vector3 worldposition;
+        public int gridX;
+        public int gridY;
 
-        public Node(bool _walkable, Vector3 _worldposition)
+        public int gCost;
+        public int hCost;
+        public Node parent;
+
+        public int fCost
+        {
+            get { return gCost + hCost; }
+        }
+
+        public Node(bool _walkable, Vector3 _worldposition, int _gridX, int _gridY)
         {
             walkable = _walkable;
             worldposition = _worldposition;
+            gridX = _gridX;
+            gridY = _gridY;
         }
+      
+      
     }
 }
