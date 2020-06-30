d92e5ce81e56f3b899468bbfde2069733b39bfca
Blaide <1017007@student.sae.edu.au>
2020-06-30 16:16:23 +1000

    Basic Network UI and NetworkManager

---
+++ Assets/Student workspace/Blaide/NetworkManagerTest.cs
@@ -0,0 +1,29 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace Student_workspace.Blaide
+{
+    public class NetworkManagerTest : NetworkManager
+    {
+        public void SetHostName(string hostName)
+        {
+            networkAddress = hostName;
+        }
+
+        public override void OnStartServer()
+        {
+            Debug.Log("server started :"
+            + networkAddress);
+            
+            base.OnStartServer();
+
+        }
+
+        public override void OnClientConnect(NetworkConnection conn)
+        {
+            Debug.Log(" Connected to server at :" + networkAddress);
+            base.OnClientConnect(conn);
+        }
+        
+    }
+}
