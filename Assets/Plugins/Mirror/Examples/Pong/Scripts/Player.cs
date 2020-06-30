c94be5ce8bc7473b1db7d1ae4cafd30ae079dd3f
Cameron Bonde <c.bonde@sae.edu>
2020-06-30 14:20:49 +1000

    Pong sample update

--- Assets/Plugins/Mirror/Examples/Pong/Scripts/Player.cs
+++ Assets/Plugins/Mirror/Examples/Pong/Scripts/Player.cs
@@ -1,5 +1,7 @@
 using Mirror;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 // using UnityEngine.InputSystem;
 
 namespace Mirror.Examples.Pong
@@ -16,8 +18,8 @@ namespace Mirror.Examples.Pong
             // don't control other player's rackets
 			if (isLocalPlayer)
 			{
-				rigidbody2d.velocity = new Vector2(0, Input.GetAxisRaw("Vertical")) * speed * Time.fixedDeltaTime;
-				// rigidbody2d.velocity = new Vector2(0, InputSystem.GetDevice<Mouse>().delta.y) * speed * Time.fixedDeltaTime;
+				// rigidbody2d.velocity = new Vector2(0, Input.GetAxisRaw("Vertical")) * speed * Time.fixedDeltaTime;
+				rigidbody2d.velocity = new Vector2(0, InputSystem.GetDevice<Mouse>().delta.y.ReadValue()) * speed * Time.fixedDeltaTime;
 			}
         }
     }
