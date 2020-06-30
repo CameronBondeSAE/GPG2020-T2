3b3e1b955356f8bb8ad770425c6a465ee48c2a6a
ayounes2001 <1016744@student.sae.edu.au>
2020-06-30 14:55:31 +1000

    Using interfaces for the Inventory

---
+++ Assets/Student workspace/Anthony Younes WS/ActionMap/Scripts/IInventoryItems.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInventoryItems
+{ 
+        string objectName { get; } 
+        void OnPickUp();
+        Sprite  objectImage { get; }
+}
--- Assets/Student workspace/Anthony Younes WS/ActionMap/Scripts/InventoryBase.cs
+++ Assets/Student workspace/Anthony Younes WS/ActionMap/Scripts/InventoryBase.cs
@@ -1,13 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace AnthonyY
 {
-    public class InventoryBase : MonoBehaviour
+    public abstract class InventoryBase : MonoBehaviour,IInventoryItems
     {
-        public List<GameObject> inventoryItems;
-        protected virtual void Inventory()
+        //creating a list of inventory Items and getting its info
+        public List<IInventoryItems> inventoryItems;
+        [TextArea]
+        public string inventoryName;
+        public Sprite objectImage { get; }
+
+        public bool pickedUp;
+
+        public Event itemAddedEvent;
+
+        public string objectName
+        {
+            get { return inventoryName;}
+        }
+
+        public virtual void OnPickUp()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual void AddItem(IInventoryItems item)
         {
             
         }
