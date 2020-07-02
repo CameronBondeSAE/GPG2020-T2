4b79fa922d9b2e5eec97c7666844c955ef9e447b
ayounes2001 <1016744@student.sae.edu.au>
2020-07-02 12:02:04 +1000

    More Basic inventory system

---
+++ Assets/Student workspace/Anthony Younes WS/ActionMap/Test Scripts/InventoryTesting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using AnthonY;
+using AnthonyY;
+using UnityEngine;
+
+public class InventoryTesting : MonoBehaviour
+{
+    public Inventory inventory;
+    
+    public HealthPickUp healthPickUpTest;
+    public InventoryControl InventoryControl;
+    
+    void Awake()
+    {
+        Tests(); 
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Collider>().)
+    }
+    //A QUICK HACK FOR TESTING
+    private void Tests()
+    {
+        inventory.items.Add(healthPickUpTest);
+        inventory.items[0].OnPickUp();
+        inventory.items[0].OnActivate();
+    }
+
+}
