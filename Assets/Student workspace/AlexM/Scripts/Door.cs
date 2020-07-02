acd1187fcb44e4fcd7ded32d57bfcf3a3dcb051d
AlexM <1007808@student.sae.edu.au>
2020-07-02 16:20:13 +1000

    a 'working' door

---
+++ Assets/Student workspace/AlexM/Scripts/Door.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace alexM
+{
+    public class Door : MonoBehaviour
+    {
+        public UnityEvent openedEvent;
+        [HideInInspector] public GameObject target;
+        [HideInInspector] public Vector3 startPos, targetPos, doorPos;
+
+         public bool isOpening;
+
+        public GameObject door;
+        //[HideInInspector] public bool targetReached;
+
+        
+        // Start is called before the first frame update
+        void Awake()
+        {
+            startPos = door.transform.position;
+            if (target != null)
+            {
+                targetPos = target.transform.position;
+            }
+        }
+
+        void SetTarget(GameObject tgt)
+        {
+            if (target != null)
+            {
+                target = tgt;
+                targetPos = target.transform.position;
+            }
+        }
+
+        public void Open()
+        {
+            if (!isOpening)
+            {
+                isOpening = true;
+                Debug.Log("Opening...");
+            }
+            else
+            {
+                isOpening = false;
+                Debug.Log("Closing...");
+            }
+            
+            //Debug.Log("Im Opened");
+        }
+
+        public bool TargetReached()
+        {
+            float dist = Vector3.Distance(door.transform.position, target.transform.position);
+
+            if (dist <= 0.25)
+            {
+                openedEvent?.Invoke();
+                return true;
+            }
+            else
+            {
+                return false;    
+            }
+        }
+
+        private void Update()
+        {
+            
+            //Do move stuff here
+            if (isOpening)
+            {
+                //lerp to targetPos
+                door.transform.position = Vector3.Lerp(targetPos, door.transform.position, 0.1f).normalized;
+            }
+        }
+    }
+}
