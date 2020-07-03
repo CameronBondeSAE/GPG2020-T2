3f57764b5b556fb41ace19cd113ccacd4bc3e954
Dylan <dylanwebb5@hotmail.com>
2020-07-03 16:05:21 +1000

    Basic Network Player Movement (client side)

---
+++ Assets/Student workspace/Dylan/Scripts/Player/NetworkPlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+namespace Student_workspace.Dylan.Scripts.Player
+{
+    public class NetworkPlayerController : NetworkBehaviour
+    {
+        [SyncVar] public string playerName;
+        
+        public float moveSpeed = 5f;
+        public CharacterController controller = null;
+
+        private Vector2 previousInput;
+
+        private PlayerControl controls;
+
+        private PlayerControl Controls
+        {
+            get
+            {
+                if (controls != null)
+                {
+                    return controls;
+                }
+
+                return controls = new PlayerControl();
+            }
+        }
+
+        public override void OnStartAuthority()
+        {
+            enabled = true;
+
+            Controls.Player.Movement.performed += ctx => SetMovement(ctx.ReadValue<Vector2>());
+            Controls.Player.Movement.canceled += ctx => ResetMovement();
+        }
+
+        [ClientCallback]
+        private void OnEnable()
+        {
+            Controls.Enable();
+        }
+
+        [ClientCallback]
+        private void OnDisable()
+        {
+            Controls.Disable();
+        }
+
+        [ClientCallback]
+        private void Update()
+        {
+            Move();
+        }
+
+        [Client]
+        private void Move()
+        {
+            Vector3 right = controller.transform.right;
+            Vector3 forward = controller.transform.forward;
+            right.y = 0f;
+            forward.y = 0f;
+
+            Vector3 movement = right.normalized * previousInput.x + forward.normalized * previousInput.y;
+
+            controller.Move(movement * (moveSpeed * Time.deltaTime));
+        }
+        
+        [Client]
+        private void SetMovement(Vector2 movement)
+        {
+            previousInput = movement;
+        }
+
+        [Client]
+        private void ResetMovement()
+        {
+            previousInput = Vector2.zero;
+        }
+    }
+}
---
+++ Assets/Student workspace/Dylan/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,176 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Student workspace/Dylan/Scripts/Player/PlayerControl.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControl : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControl()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControl"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""e6f3f30e-51ca-4314-af0d-92f34afd2027"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Button"",
+                    ""id"": ""33097c04-c5d6-42a0-8a22-a5a637e57a51"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""b20106c5-d492-4819-90e6-10a85026ba06"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""ce0dc5e5-8246-4d3e-83a5-550ab8d00d6a"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""1b29634f-8cd0-4dc1-92e6-bbc763063381"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""5621a3d6-fca8-47fc-b00e-5b34cc1764b0"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""8fdfb026-2bb2-4058-ae66-497c6329302a"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Movement;
+    public struct PlayerActions
+    {
+        private @PlayerControl m_Wrapper;
+        public PlayerActions(@PlayerControl wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+    }
+}
