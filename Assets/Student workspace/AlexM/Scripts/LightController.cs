946ed4eebd91c9d7c142a98b03bca1b2362fbe28
AlexM <1007808@student.sae.edu.au>
2020-07-04 15:55:04 +1000

    scene:DoorTest:added some lights that react to the various states of the door, also added a LightController script, with an editor mod script to test it out aswell.

---
+++ Assets/Student workspace/AlexM/Scripts/LightController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+     public bool toggleState;
+    [HideInInspector] public float intensity;
+
+    [HideInInspector] public Light light;
+
+    void Awake()
+    {
+        light = gameObject.GetComponent<Light>();
+        //toggleState = gameObject.activeSelf;
+        intensity = light.intensity;
+    }
+
+    private void Update()
+    {
+        if (toggleState)
+        {
+           //Toggle Off
+           light.intensity = intensity;
+
+        }
+        else
+        {
+            //light.gameObject.SetActive(true);
+            light.intensity = 0;
+        }
+    }
+
+    public void TurnOn()
+    {
+        toggleState = true;
+    }
+
+    public void TurnOff()
+    {
+        toggleState = false;
+    }
+
+
+    public void Toggle()
+    {
+        if (toggleState)
+        {
+            toggleState = false;
+        }else if (!toggleState)
+        {
+            toggleState = true;
+        }
+    }
+   
+}
