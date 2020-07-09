c3d8bc702b0b54b9a1963eba36309e54ad149208
AlexM <1007808@student.sae.edu.au>
2020-07-09 11:49:24 +1000

    just testing some git commands!

--- Assets/Student workspace/AlexM/Scripts/PlayerController.cs
+++ Assets/Student workspace/AlexM/Scripts/PlayerController.cs
@@ -6,46 +6,39 @@ using UnityEngine.InputSystem;
 
 namespace alexM
 {
-    public class PlayerController : MonoBehaviour
-    {
-        public float sensitivity;
-        private float _xRot, _yRot, _mouseX, _mouseY;
-        public Transform neckJoint;
-        public GameControls gc;
+	public class PlayerController : MonoBehaviour
+	{
+		public  float        sensitivity;
+		private float        _xRot, _yRot, _mouseX, _mouseY;
+		public  Transform    neckJoint;
+		public  GameControls gc;
 
-        private void Awake()
-        {
-             gc = new GameControls();
-             gc.InGame.Enable();
-        }
+		private void Awake()
+		{
+			gc = new GameControls();
+			gc.InGame.Enable();
+		}
 
-        void MouseLook()
-        {
-             _mouseX += (InputSystem.GetDevice<Mouse>().delta.x.ReadValue() * sensitivity ) * Time.deltaTime;
-             _mouseY += (InputSystem.GetDevice<Mouse>().delta.y.ReadValue() * sensitivity ) * Time.deltaTime;
+		void MouseLook()
+		{
+			_mouseX += (InputSystem.GetDevice<Mouse>().delta.x.ReadValue() * sensitivity) * Time.deltaTime;
+			_mouseY += (InputSystem.GetDevice<Mouse>().delta.y.ReadValue() * sensitivity) * Time.deltaTime;
 
-            _xRot = _mouseY;
-            _xRot = Mathf.Clamp(_xRot, -50, 50);
-            
-             _yRot = _mouseX;
-             _yRot = Mathf.Clamp(_yRot, -50, 50);
-            
-            //neckJoint.Rotate(Vector3.up * mouseX);
-            neckJoint.transform.rotation = Quaternion.Euler(-_xRot, _yRot ,0);
+			_xRot = _mouseY;
+			_xRot = Mathf.Clamp(_xRot, -50, 50);
 
-            Debug.Log("x: " + _mouseX + " y: " + _mouseY);
+			_yRot = _mouseX;
+			_yRot = Mathf.Clamp(_yRot, -50, 50);
 
-            // xRot -= mouseY;
-            // xRot = Mathf.Clamp(xRot, -90, 90);
-            //
-            // neckJoint.Rotate(Vector3.up * xRot);
+			neckJoint.transform.rotation = Quaternion.Euler(-_xRot, _yRot, 0);
 
-        }
+			//Debug.Log("x: " + _mouseX + " y: " + _mouseY);
+		}
 
 
-        private void FixedUpdate()
-        {
-            MouseLook();
-        }
-    }
+		private void FixedUpdate()
+		{
+			MouseLook();
+		}
+	}
 }
