d6c32a2fdac8f11ac415570848af0b6ff3206a99
Cameron Bonde <c.bonde@sae.edu>
2020-07-09 15:14:21 +1000

    Examples of delegating functionality from a health component, inheritance VS events. Uses UnityEvent, not C# but it'd be very similar

---
+++ Assets/Tutorial examples/Events vs Inheritance/BarrelUsingInspectorEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelUsingInspectorEvent : MonoBehaviour
+{
+
+
+	public void ThingToDoWhenIDie()
+	{
+		Debug.Log("I'm a barrel using the inspector to hook up the Die event... and I'm dead now");
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Tutorial examples/Events vs Inheritance/HealthUsingEvents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthUsingEvents : MonoBehaviour
+{
+	public float current;
+	
+	public UnityEvent dieEvent;
+
+	public void Die()
+	{
+		dieEvent.Invoke();
+	}
+}
