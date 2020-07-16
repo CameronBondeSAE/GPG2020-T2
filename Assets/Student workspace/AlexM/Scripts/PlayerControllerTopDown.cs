bf99550e62ff803edc4e4bc63ccd14abbc961de2
AlexM <1007808@student.sae.edu.au>
2020-07-16 10:30:26 +1000

    Top-Down player basic functionality is there

--- Assets/Student workspace/AlexM/Scripts/PlayerControllerTopDown.cs
+++ Assets/Student workspace/AlexM/Scripts/PlayerControllerTopDown.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,18 +10,21 @@ namespace alexM
 	public class PlayerControllerTopDown : MonoBehaviour
 	{
 		public  float        speed;
-		public Vector3 direction;
+		public  Vector3      direction;
 		private GameControls GC;
 		private Rigidbody    RB;
-
+		private Camera       camera;
+		private float        camOffset;
 
 		private void Awake()
 		{
 			GC = new GameControls();
 			GC.Enable();
 			GC.InGame.Move.performed += Movement;
-			GC.InGame.Move.canceled += Movement;
-			RB                     =  GetComponent<Rigidbody>();
+			GC.InGame.Move.canceled  += Movement;
+			RB                       =  GetComponent<Rigidbody>();
+			camera                   =  GetComponentInChildren<Camera>();
+			camOffset                =  Vector3.Distance(camera.transform.position, gameObject.transform.position);
 		}
 
 		void Movement(InputAction.CallbackContext context)
@@ -29,30 +33,24 @@ namespace alexM
 
 			direction = context.ReadValue<Vector2>();
 			direction = new Vector3(direction.x, 0, direction.y);
-
-
-			// if (context.phase == InputActionPhase.Performed)
-			// {
-			// 	direction = context.ReadValue<Vector3>();
-			// 	//Debug.Log("HOLDING KEY!");
-			// }
-			//
-			// if (context.phase == InputActionPhase.Canceled)
-			// {
-			// 	direction = context.ReadValue<Vector3>();
-			// 	Debug.Log("LET GO OF KEY!");
-			// }
-			//
-			//Vector2 inputVal = context.ReadValue<Vector2>();
-			//RB.velocity = new Vector3((inputVal.x * speed) * Time.deltaTime, 0, (inputVal.y * speed) * Time.deltaTime);
 		}
 
 		private void Update()
 		{
 			//AirSpeed control (Check for ground and set speed to airSpeed [Slower])
-			
+
 			//Do the stuff here
 			RB.AddForce(direction * speed);
+		}
+
+		private void FixedUpdate()
+		{
+		}
+
+		private void OnDestroy()
+		{
+			GC.InGame.Move.performed -= Movement;
+			GC.InGame.Move.canceled  -= Movement;
 		}
 	}
 }
