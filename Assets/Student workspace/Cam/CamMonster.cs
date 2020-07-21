e70b9fbd20f5ade89af9ff20323dcbba7747202f
Cameron Bonde <c.bonde@sae.edu>
2020-07-21 14:52:02 +1000

    VFX graph tests and examples

--- Assets/Student workspace/Cam/CamMonster.cs
+++ Assets/Student workspace/Cam/CamMonster.cs
@@ -1,5 +1,13 @@
-﻿using AJ;
+﻿using System;
+using AJ;
 using UnityEngine;
+
+public class DidSomethingCoolEventArgs : EventArgs
+{
+	public int things;
+	public string stuff;
+	public bool woo;
+}
 
 public class CamMonster : CharacterBase
 {
@@ -7,9 +15,30 @@ public class CamMonster : CharacterBase
 
 	public bool shield;
 
+
+	
+	// Using Action generics
+	public event Action<object, EventArgs> didSomethingCool ;
+	
+	
+	// Using manually defined delegate
+	public delegate void DidSomethingCoolDelegate(object sender, EventArgs e);
+	public event DidSomethingCoolDelegate DidSomethingCool;
+	
+	
+	
+	
 	private void Start()
 	{
 		GetComponent<HealthComponent>().deathEvent.AddListener(Die);
+		
+		DidSomethingCool += OnDidSomethingCool;
+	}
+
+	private void OnDidSomethingCool(object sender, EventArgs e)
+	{
+		DidSomethingCoolEventArgs args = e as DidSomethingCoolEventArgs;
+		Debug.Log(args.stuff);
 	}
 
 	private void OnDestroy()
@@ -22,6 +51,13 @@ public class CamMonster : CharacterBase
 	{
 		// TODO: Put in the real player distance (multiple players?)
 		// animator.SetFloat("DistanceToPlayer", Vector3.Distance(transform.position, Vector3.zero));
+		
+		DidSomethingCool.Invoke(this, new DidSomethingCoolEventArgs
+									  {
+										  things = 5,
+										  stuff  = "Cam",
+										  woo    = true
+									  });
 	}
 
 
