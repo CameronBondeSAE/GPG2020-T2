dc5f53325aaa04483c56b7d7998836815117651a
AJ <1018212@student.sae.edu.au>
2020-07-29 13:42:15 +1000

    Gun in progress

---
+++ Assets/Student workspace/AlexB/Scripts/ShootEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class ShootEditor : Editor
+{
+    [CustomEditor(typeof(Shoot))]
+
+    public class Shoot : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if (GUILayout.Button("Shoot"))
+            {
+                //((Shoot)target);
+            }
+        }
+    }
+}
