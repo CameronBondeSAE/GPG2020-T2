1e40c13dbef9524d10052183c8d4d65042521acd
ayounes2001 <1016744@student.sae.edu.au>
2020-08-05 16:22:42 +1000

    Basic idea of the speed component

---
+++ Assets/Student workspace/Anthony Younes WS/Speed.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
+
+public class Speed : MonoBehaviour
+{
+    public bool GimmeRigidBody;
+    public Rigidbody rb;
+    private Vector3 previousPosition;
+
+    [SerializeField]
+    private float speed;
+
+    public float Speed1
+    {
+        get
+        {
+            if (!rb)
+            {
+                if (GimmeRigidBody)
+                {
+                    speed = rb.velocity.magnitude;
+                    if (!gameObject.GetComponent<Rigidbody>())
+                    {
+                        gameObject.GetComponent<Rigidbody>();
+                        rb = gameObject.GetComponent<Rigidbody>();
+                        rb.constraints = RigidbodyConstraints.FreezeRotationX;
+                        rb.constraints = RigidbodyConstraints.FreezePositionZ;
+                    
+                    }
+                    else
+                    {
+                        if (gameObject.GetComponent<Rigidbody>())
+                        {
+                            rb = gameObject.GetComponent<Rigidbody>();
+                        }
+                    }
+                        
+
+                    speed = Vector3.Distance(previousPosition, transform.position)/Time.deltaTime;
+                    previousPosition = transform.position;
+                    
+                }
+            }
+             return speed;
+        }
+        set
+        {
+            speed = value;
+        } 
+    }
+    
+}
