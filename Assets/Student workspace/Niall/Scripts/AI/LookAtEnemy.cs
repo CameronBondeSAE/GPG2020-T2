e09f4adb8905aa1668b3a94ce98fb9ed5bc31080
Niall <1013734@student.sae.edu.au>
2020-08-11 16:19:19 +1000

    Turn Towards + line of sight.
    line of sight now looks towards target, different script than anthony's

--- Assets/Student workspace/Niall/Scripts/AI/LookAtEnemy.cs
+++ Assets/Student workspace/Niall/Scripts/AI/LookAtEnemy.cs
@@ -3,24 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LookAtEnemy : MonoBehaviour
+namespace Niall 
 {
-  //  public float speed;
-    public Transform FollowPos;
-    public float force = 0.1f;
-
-
-    public void Update()
+    public class LookAtEnemy : MonoBehaviour
     {
-       // Quaternion rotTarget = Quaternion.LookRotation(FollowPos.position - this.transform.position);
-      //  this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, rotTarget, speed * Time.deltaTime);
 
-      Vector3 targetDelta = FollowPos.position - transform.position;
+        public Transform FollowPos;
+        public float force = 0.1f;
+        private LineOfSight lineOfSight;
 
-      float angleDiff = Vector3.Angle(transform.forward, targetDelta);
 
-      Vector3 cross = Vector3.Cross(transform.forward, targetDelta);
+        public void Start()
+        {
+            lineOfSight = GetComponent<LineOfSight>();
+        }
 
-      GetComponent<Rigidbody>().AddTorque(cross * (angleDiff * force));
+        public void Update()
+        {
+          //  if (lineOfSight.haveLOS)
+          //  {
+                Vector3 targetDelta = FollowPos.position - transform.position;
+
+                float angleDiff = Vector3.Angle(transform.forward, targetDelta);
+
+                Vector3 cross = Vector3.Cross(transform.forward, targetDelta);
+
+                GetComponent<Rigidbody>().AddTorque(cross * (angleDiff * force));
+          //  }
+        }
     }
 }
