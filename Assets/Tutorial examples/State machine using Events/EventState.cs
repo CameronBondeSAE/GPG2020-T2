2245ae16e1a356506cb6d0db369d7fcdaa5ecaf0
Cameron Bonde <c.bonde@sae.edu>
2020-08-14 16:35:22 +1000

    Updated debug prefab with player

--- Assets/Tutorial examples/State machine using Events/EventState.cs
+++ Assets/Tutorial examples/State machine using Events/EventState.cs
@@ -1,33 +1,12 @@
 using System;
 
-
 [Serializable]
 public class EventState
 {
 	// Action just means 'any function'. So these variables can point to a function
 	// Action is a shortcut for this    "public delegate void Enter();"
-	
-	public event Action DoThing;
-
-	public delegate void CamsDelegate(int number);
-
-	public CamsDelegate withInt;
-	
 	public Action Enter;
 	public Action Execute;
 	public Action Exit;
 	public bool   active;
-
-	public void Thing()
-	{
-		withInt += WithInt;
-		
-		withInt?.Invoke(4);
-		
-	}
-
-	private void WithInt(int number)
-	{
-		throw new NotImplementedException();
-	}
 }
