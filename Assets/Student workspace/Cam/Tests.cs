dd4a90df6a9a188550eea3a7271adc585299a6f1
Cameron Bonde <c.bonde@sae.edu>
2020-08-14 15:21:50 +1000

    Cams level

--- Assets/Student workspace/Cam/Tests.cs
+++ Assets/Student workspace/Cam/Tests.cs
@@ -1,6 +1,7 @@
 using System;
 using Mirror;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Student_workspace.Cam
 {
@@ -20,13 +21,39 @@ namespace Student_workspace.Cam
 			}
 		}
 
+		public LayerMask lm;
 
 		private void Start()
 		{
+			
+			
 			GameObject go = new GameObject();
 
 			go.AddComponent<Rigidbody>();
 			go.AddComponent<Collider>();
+
+
+			Debug.Log(IsInLayerMask(gameObject.layer, lm));
+			
+			GameControls gameControls = new GameControls();
+			gameControls.InGame.Fire.performed += MouseLookOnperformed;
+			gameControls.InMenu.Newaction.performed += NewactionOnperformed;
+
+			gameControls.InGame.Enable();
+			gameControls.InMenu.Disable();
+		}
+
+		private void NewactionOnperformed(InputAction.CallbackContext obj)
+		{
+		}
+
+		private void MouseLookOnperformed(InputAction.CallbackContext obj)
+		{
+		}
+
+		public bool IsInLayerMask(int layer, LayerMask layermask)
+		{
+			return layermask == (layermask | (1 << layer));
 		}
 	}
 }
