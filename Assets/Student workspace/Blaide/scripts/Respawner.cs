62569c1470e43ea276787c1a0179aea141f86aa4
Blaide <1017007@student.sae.edu.au>
2020-08-20 14:21:35 +1000

    Respawn System

---
+++ Assets/Student workspace/Blaide/scripts/Respawner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Collider))]
+public class Respawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Respawnable r;
+        if (other.transform.root.TryGetComponent<Respawnable>(out r))
+        {
+            r.Respawn();
+        }
+    }
+}
