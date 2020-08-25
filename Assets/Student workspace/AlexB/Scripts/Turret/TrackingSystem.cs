2ab3f81db33217ecb0a287eca78d63ea8fa6fcba
AJ <1018212@student.sae.edu.au>
2020-08-25 16:28:14 +1000

    Still working on the turret

---
+++ Assets/Student workspace/AlexB/Scripts/Turret/TrackingSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AJ
+{
+    public class TrackingSystem : MonoBehaviour
+    {
+        public float speed = 3.0f;
+        public GameObject m_target = null;
+        Vector3 m_lastKnownPosition = Vector3.zero;
+        Quaternion m_lookAtRotation;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (m_target)
+            {
+                if (m_lastKnownPosition != m_target.transform.position)
+                {
+                    m_lastKnownPosition = m_target.transform.position;
+                    m_lookAtRotation = Quaternion.LookRotation(m_lastKnownPosition - transform.position);
+                }
+                if (transform.rotation != m_lookAtRotation)
+                {
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, m_lookAtRotation, speed * Time.deltaTime);
+                }
+            }
+        }
+
+        private bool SetTarget(GameObject target)
+        {
+            if (target)
+            {
+                return false;
+            }
+
+            m_target = target;
+
+            return true;
+        }
+    }
+}
