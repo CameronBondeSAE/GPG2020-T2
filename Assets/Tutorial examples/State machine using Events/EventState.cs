64acdea8354348b5762d027af1994c1d1d8830d5
Cameron Bonde <c.bonde@sae.edu>
2020-08-25 17:17:00 +1000

    State machine setup and example tweaks

--- Assets/Tutorial examples/State machine using Events/EventState.cs
+++ Assets/Tutorial examples/State machine using Events/EventState.cs
@@ -3,6 +3,7 @@ using System;
 [Serializable]
 public class EventState
 {
+	// Variables can POINT TO FUNCTIONS! It's just another piece of information, like numbers, strings etc. A function name is just another type of info really.
 	// Action just means 'any function'. So these variables can point to a function
 	// Action is a shortcut for this    "public delegate void Enter();"
 	public Action Enter;
