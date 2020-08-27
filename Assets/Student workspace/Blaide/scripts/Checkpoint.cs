20ee496707f92a120baae2476a44277872ce612d
Blaide <1017007@student.sae.edu.au>
2020-08-27 12:09:40 +1000

    Basic Respawn, Basic Checkpoints, menu stuff.

---
+++ Assets/Student workspace/Blaide/scripts/Checkpoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Checkpoint : MonoBehaviour
+{
+    public Vector3 point;
+
+    public UnityEvent reached;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        Respawnable res;
+        Component comp;
+
+            if (other.gameObject.transform.root.TryGetComponent(typeof(Respawnable), out comp))
+            {
+                res = (Respawnable) comp;
+                res.SetNewRespawnPoint(transform.position);
+                reached.Invoke();
+
+            }
+    }
+}
