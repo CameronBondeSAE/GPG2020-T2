245ab7b7a94cea2fe563a5a342510980d4ea14e2
AJ <1018212@student.sae.edu.au>
2020-09-01 17:27:16 +1000

    turret statemachine works

--- Assets/Student workspace/AlexB/Scripts/Turret/TrackingSystem.cs
+++ Assets/Student workspace/AlexB/Scripts/Turret/TrackingSystem.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Niall;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,6 +12,9 @@ namespace AJ
         Vector3 lastKnownPosition = Vector3.zero;
         Quaternion lookAtRotation;
 
+        public LineOfSight lineOfSight;
+        public Nearby nearby;
+
         private EventStateManager stateManager;
         public EventState SearchState;
         public EventState AttackState;
@@ -20,22 +24,44 @@ namespace AJ
             stateManager = new EventStateManager();
             SearchState = new EventState();
             SearchState.Execute = SearchStateUpdate;
+            AttackState = new EventState();
+            AttackState.Execute = AttackStateUpdate;
             stateManager.ChangeState(SearchState);
         }
 
         private void Update()
         {
+            
             stateManager.ExecuteCurrentState();
         }
 
         void SearchStateUpdate()
         {
-            stateManager.ChangeState(AttackState);
+            transform.Rotate(new Vector3(0f, 100f, 0f) * Time.deltaTime);
+            if (lineOfSight.haveLOS == true)
+            {
+                stateManager.ChangeState(AttackState);
+            }
+
+            if(nearby.GetClosest() != null)
+            {
+                lineOfSight.targets = new List<Transform>();
+
+                lineOfSight.targets.Add(nearby.GetClosest().transform);
+                
+                if (lineOfSight.haveLOS == true)
+                {
+                    stateManager.ChangeState(AttackState);
+                }
+            }
+            
+
         }
 
         // Update is called once per frame
         void AttackStateUpdate()
         {
+            //make target = nearby
             if (target)
             {
                 if (lastKnownPosition != target.transform.position)
@@ -47,6 +73,11 @@ namespace AJ
                 {
                     transform.rotation = Quaternion.RotateTowards(transform.rotation, lookAtRotation, speed * Time.deltaTime);
                 }
+            }
+
+            if (nearby.GetClosest() == null)
+            {
+                stateManager.ChangeState(SearchState);
             }
         }
 
