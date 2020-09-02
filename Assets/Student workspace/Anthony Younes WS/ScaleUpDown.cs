483948ee2c56eaa52217d76dc97e25cf9a65fc22
ayounes2001 <1016744@student.sae.edu.au>
2020-09-02 11:28:01 +1000

    Scale Up and Down Component

---
+++ Assets/Student workspace/Anthony Younes WS/ScaleUpDown.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AnthonyY
+{
+    public class ScaleUpDown : MonoBehaviour
+    { 
+        public float length = 3;
+   
+        void Update()
+        {
+            transform.localScale = new Vector3(Mathf.PingPong(Time.time,length),transform.localScale.y,transform.localScale.z);
+        }
+    }
+}
