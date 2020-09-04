6f012fecbd39527e2239b9bb37c89ebcb3ed2bba
Blaide <1017007@student.sae.edu.au>
2020-09-04 16:50:20 +1000

    SpawnPoint Roundrobin. incase players > spawnpoints.

--- Assets/Student workspace/Blaide/Interfaces/IPossessable.cs
+++ Assets/Student workspace/Blaide/Interfaces/IPossessable.cs
@@ -4,12 +4,29 @@ using Student_workspace.Dylan.Scripts.NetworkLobby;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// This is an Interface used specifically for the networkGamePlayer possessing, Essentially Controls are handled by passing through this to whatever the network player should be controlling.
+/// </summary>
 public interface IPossessable
 { 
+    
     NetworkGamePlayer possessor{ get; set; }
-
+/// <summary>
+/// Movement control passed from GameNetworkPlayer.
+/// </summary>
+/// <param name="dir">Just a shortcut for ctx.ReadValue </param>
+/// <param name="ctx">Passed through callback context from the inputsystem, Use this to determine the input phase for performed or canceled etc.</param>
     void Movement(Vector2 dir,InputAction.CallbackContext ctx);
+/// <summary>
+/// Aiming control passed from GameNetworkPlayer.
+/// </summary>
+/// <param name="Pos">Just a shortcut for ctx.ReadValue </param>
+/// <param name="ctx">Passed through callback context from the inputsystem, Use this to determine the input phase for performed or canceled etc.</param>
     void Aiming(Vector2 pos,InputAction.CallbackContext ctx);
+/// <summary>
+/// Fire Action passed from GameNetworkPlayer.
+/// </summary>
+/// <param name="ctx"></param>
     void Fire(InputAction.CallbackContext ctx);
     void Jump(InputAction.CallbackContext ctx);
     void Interact(InputAction.CallbackContext ctx);
